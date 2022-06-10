6a7ee2d554df750e8b1e14470c4f9d662506d04b
lmnoppy <code@lmnoppy.com>
2022-06-10 21:03:41 +0100

    tutorial 1 set up

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <p>About... bla bla bla</p>
+    </>
+  )
+}
+
+export default AboutPage
